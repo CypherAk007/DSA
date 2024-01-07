766341bf03c1c1fcc39340e5a5c85a2ff6f9ac03
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-07 17:58:44 +0530

    height of bst and check balanced

--- Trees/07_insertIntoBST.py
+++ Trees/07_insertIntoBST.py
@@ -3,6 +3,7 @@ class Node:
         self.value = value
         self.left = None 
         self.right = None
+        self.height = 0
 
 class BST:
     def __init__(self) -> None:
@@ -17,8 +18,22 @@ class BST:
             return 
         print(details+str(node.value))
         self.displayIntenal(node.left,"LeftChildOf "+str(node.value)+": ") 
-        self.displayIntenal(node.right,"RightChildOf "+str(node.value)+": ") 
+        self.displayIntenal(node.right,"RightChildOf "+str(node.value)+": ")
+
+    def height(self,node):
+        if node==None:
+            return -1
+        return node.height
     
+    def balanced(self):
+        return self.balancedInternal(self.root)
+    
+    def balancedInternal(self,node):
+        if node==None:
+            return True 
+        return abs(self.height(node.left)-self.height(node.right))<=1 and self.balancedInternal(node.left) and self.balancedInternal(node.right)
+    
+
     def insert(self,value):
         self.root = self.insertInternal(value,self.root)
 
@@ -33,11 +48,33 @@ class BST:
         if value>=node.value:
             node.right = self.insertInternal(value,node.right)
 
+        node.height = max(self.height(node.left),self.height(node.right))+1
+
         return node
+    
+
 bst = BST()
 bst.insert(5)
+print("height",bst.height(bst.root))
 bst.insert(4)
+print("height",bst.height(bst.root))
 bst.insert(6)
 bst.insert(3)
+bst.insert(2)
+print("height",bst.height(bst.root))
 bst.display()
+print(bst.balanced())
+'''
+output
+height 0
+height 1
+height 3
+Root Node : 5
+LeftChildOf 5: 4
+LeftChildOf 4: 3
+LeftChildOf 3: 2
+RightChildOf 5: 6
+False
+
+'''
         
